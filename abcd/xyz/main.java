91937fc9b473030c04d3a013c5d386771d63a698
Rahul Kumar <ayushrahul8888@gmail.com>
2021-05-20 17:57:43 +0530

    added main file

---
+++ abcd/xyz/main.java
@@ -0,0 +1,27 @@
+package abcd.xyz;
+
+public class main {
+	public static void main(String[] args) {
+
+
+	    cuboid first = new cuboid();
+	    first.vol();
+	    first.surface();
+	    //2nd
+	    cuboid second = new cuboid();
+	    second.vol();
+	    second.surface();
+	    //3rd
+	    cuboid third = new cuboid();
+	    third.vol();
+	    third.surface();
+	    //4th
+	    cuboid fourth = new cuboid();
+	    fourth.vol();
+	    fourth.surface();
+
+
+
+	    }
+
+}
