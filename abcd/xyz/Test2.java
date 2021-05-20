015291ca6e3029b69245a461b51c6da8ea8f0f0e
Rahul Kumar <ayushrahul8888@gmail.com>
2021-05-20 17:56:54 +0530

    added cuboid class

--- abcd/xyz/Test2.java
+++ abcd/xyz/Test2.java
@@ -1,5 +1,39 @@
 package abcd.xyz;
 
-public class Test2 {
 
-}
+
+	import java.util.*;
+	class cuboid
+	{
+
+	    private final int l;
+	    private final int b;
+	    private final int h;
+	    public  cuboid()
+	    {
+	        Scanner scan = new Scanner(System.in);
+	        System.out.println("enter length");
+	        l=scan.nextInt();
+	        System.out.println("enter breadth");
+	        b=scan.nextInt();
+	        System.out.println("enter height");
+	        h=scan.nextInt();
+
+	    }
+	    public int v;
+	    public int s;
+
+
+
+	    public void vol()
+	    {
+	        v=l*b*h;
+	        System.out.println(v);
+	    }
+	    public void surface()
+	    {
+	        s= l*b+l*b +b*h +b*h+h*l+h*l ;
+	        System.out.println(s);
+	    }
+
+	}
